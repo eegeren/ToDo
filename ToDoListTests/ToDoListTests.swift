f2b59c6582aee11e0c5022fede6f3d1c90b6fd79
Yusufege Eren <egeevrakk@gmail.com>
2024-09-21 20:04:50 +0300

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Yusufege Eren on 21.09.2024.
+//
+
+import Testing
+@testable import ToDoList
+
+struct ToDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
